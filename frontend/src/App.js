3903bd0ef7ac5ec878ab7199deddcd946e006eb0
yilmazlyn <yilmaz.putun@hotmail.com>
2020-11-30 09:47:50 +0100

    on going for user profile etc...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,9 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen.js'
+import RegisterScreen from './screens/RegisterScreen'
 import LoginScreen from './screens/LoginScreen.js'
+import ProfileScreen from './screens/ProfileScreen'
 
 const App = () => {
   return (
@@ -14,7 +16,9 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/login' component={LoginScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           {/* ? operator is to say that, id is optional, means that, cart page accessible by id or without */}
           <Route path='/cart/:id?' component={CartScreen} />
