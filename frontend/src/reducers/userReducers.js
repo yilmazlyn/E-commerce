c8f94c2c4fa911710ee96db1a4f58b9959888c43
yilmazlyn <yilmaz.putun@hotmail.com>
2021-02-01 11:00:01 +0100

    admin edit user tested, redux reset page after updateUser works too

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -20,6 +20,10 @@ import {
   USER_DELETE_REQUEST,
   USER_DELETE_SUCCESS,
   USER_DELETE_FAIL,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAIL,
+  USER_UPDATE_RESET,
 } from '../constants/userConstants'
 
 //State is initialized for default state whih is empty object before any action
@@ -106,6 +110,24 @@ export const userDeleteReducer = (state = {}, action) => {
     case USER_DELETE_FAIL:
       return { loading: false, error: action.payload }
 
+    default:
+      return state
+  }
+}
+
+export const userUpdateReducer = (state = { user: {} }, action) => {
+  // Passing empty user object in state
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_SUCCESS:
+      return { loading: false, success: true }
+    case USER_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case USER_UPDATE_RESET:
+      return {
+        user: {},
+      }
     default:
       return state
   }
